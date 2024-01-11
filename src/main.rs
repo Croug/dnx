ea6ad769cac9d6b58da3dc8593537f7c487a960e
Jordan <jlaprise@blakwurm.com>
2024-01-11 13:34:42 -0900

    Add logging statements to main.rs

--- src/main.rs
+++ src/main.rs
@@ -2,11 +2,13 @@
 async fn main() {
     env_logger::init();
 
+    log::info!("Starting DNX server");
     let mut server = dnx_rs::server::setup_server().await.unwrap();
+    log::info!("DNX server started, press Ctrl+C to exit");
 
     tokio::signal::ctrl_c().await.unwrap();
 
+    log::info!("Shutting down DNX server");
     server.shutdown_gracefully().await.expect("Failed to shutdown gracefully");
-
-    println!("Goodbye!");
+    log::info!("Goodbye!");
 }
