7804ba24d41ae1d175e94adfeb41a1136ed736be
Jordan <jlaprise@blakwurm.com>
2024-01-11 15:46:10 -0900

    Add windows service status updates

--- src/service/main.rs
+++ src/service/main.rs
@@ -1,17 +1,29 @@
-use std::{ffi::OsString, sync::{Arc, atomic::AtomicBool}};
+use std::{ffi::OsString, sync::{Arc, atomic::AtomicBool}, time::Duration};
 use windows_service::{
     define_windows_service,
     service_dispatcher,
-    service::ServiceControl,
-    service_control_handler::{self, ServiceControlHandlerResult},
+    service::{ServiceControl, ServiceStatus, ServiceType, ServiceState, ServiceExitCode, ServiceControlAccept},
+    service_control_handler::{self, ServiceControlHandlerResult, ServiceStatusHandle},
 };
 
 define_windows_service!(ffi_service_main, service_main);
 
+fn update_status(current_state: ServiceState, status_handle: &ServiceStatusHandle) -> windows_service::Result<()> {
+    status_handle.set_service_status(ServiceStatus {
+        service_type: ServiceType::OWN_PROCESS,
+        current_state,
+        controls_accepted: ServiceControlAccept::STOP,
+        exit_code: ServiceExitCode::Win32(0),
+        checkpoint: 0,
+        wait_hint: Duration::default(),
+        process_id: None,
+    })
+}
+
 fn service_main(_: Vec<OsString>) {
     let signal = Arc::new(AtomicBool::new(false));
     let signal_dup = signal.clone();
-    _ = service_control_handler::register("DnxHostService", move |event| {
+    let status_handle = service_control_handler::register("DnxHostService", move |event| {
         match event {
             ServiceControl::Stop => {
                 signal_dup.store(true, std::sync::atomic::Ordering::Relaxed);
@@ -20,23 +32,29 @@ fn service_main(_: Vec<OsString>) {
             ServiceControl::Interrogate => ServiceControlHandlerResult::NoError,
             _ => ServiceControlHandlerResult::NotImplemented,
         }
-    });
+    }).expect("Failed to register service control handler");
 
     tokio::spawn(async move {
         log::info!("Starting DNX server");
         let mut server = dnx_rs::server::setup_server().await.unwrap();
         log::info!("DNX server started");
 
+        update_status(ServiceState::Running, &status_handle)
+            .expect("failed to update service status");
+
         while !signal.load(std::sync::atomic::Ordering::Relaxed) { }
         log::info!("Shutting down DNX server");
 
         server.shutdown_gracefully().await.expect("Failed to shutdown gracefully");
         log::info!("Goodbye!");
+
+        update_status(ServiceState::Stopped, &status_handle)
+            .expect("failed to update service status");
     });
 }
 
 fn main() -> Result<(), windows_service::Error> {
-    service_dispatcher::start("DnxHostService", ffi_service_main)?;
+    let status_handle = service_dispatcher::start("DnxHostService", ffi_service_main)?;
 
     Ok(())
 }
